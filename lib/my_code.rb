297aa6963afafc176f22c823cbd4e0ff83498a43
Ben Dunjay <bendunjay@outlook.com>
2020-02-21 14:46:47 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,36 @@
-# Your Code Here
+require 'pry'
+
+def map(array) 
+  new_array = []
+  counter = 0 
+  
+  while counter < array.length do 
+new_array.push(yield array[counter])
+
+
+counter += 1 
+end
+new_array
+end
+
+
+def reduce(array, starting_value = nil)
+if starting_value
+ num1 = starting_value
+ i = 0 
+ 
+ else 
+num1 = array[0] 
+i = 1 
+
+end
+
+while i < array.length do 
+  
+num1 = yield(num1, array[i])
+
+i += 1
+end 
+num1
+  
+end
