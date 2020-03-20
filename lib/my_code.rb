c9cf502923a2366a85dfdfad06435f21d36f7a14
minamalaj <mina.malaj@hotmail.com>
2020-03-20 20:21:50 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,32 @@
-# Your Code Here
+def map(array) 
+new = []
+i = 0 
+while i < array.length 
+        new.push(yield(array[i]))
+        i += 1
+     end 
+    new
+end 
+    
+def reduce(array, optval=nil)
+    if optval
+        sum = optval
+        i = 0 
+    else 
+        sum = array[0]
+        i = 1
+    end 
+    while i < array.length
+       sum = yield(sum, array[i])
+    i += 1 
+end 
+sum 
+end 
+
+
+
+
+    # do |array| 
+    #     total = array * -1
+    #     yield(total)
+    #   end 
